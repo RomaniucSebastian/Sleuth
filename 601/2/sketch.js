68b3c7a3525ce521e3d8dd67b8a5dc12a940ddd9
RomaniucSebastian <83336348+RomaniucSebastian@users.noreply.github.com>
2024-11-27 14:02:14 +0200

    Add files via upload

---
+++ 601/2/sketch.js
@@ -0,0 +1,86 @@
+/*
+Officer: 5560319
+CaseNum: 601-1-53704371-5560319
+
+Case 601 - Cross Reference - stage 2
+
+Fry is still on the loose. We think she’s resorted to stealing to get by.
+Hopefully we can track her down by cross-referencing sightings and recent thefts in the area.
+
+In the setup function, use a for loop to traverse the sightings, marking all of the locations on the map
+where she was last seen. Do this by drawing small, MediumVioletRed fill ellipses at each location.
+
+In addition, we've assembled a list of recent thefts in the area. Using another for loop to traverse the
+recent crime records, you should mark those locations on the map. Do this by drawing small, ForestGreen fill triangles centered over each location.
+
+Use X11 colours. You can find a reference table at https://www.w3.org/TR/css3-iccprof#numerical.
+
+For this mission you will need ONLY the following:
+
+- for loop
+- ellipse()
+
+- triangle() NB. Draw each triangle with the point roughly at its center.
+
+- stroke(), fill() - rgb values only
+- noStroke(), noFill()
+
+*/
+
+var countyMap;
+
+//Sightings of Casey Fry.
+
+var fryLog = {
+	LocationX: [639, 681, 712, 756, 715, 701, 753, 815, 795, 788, 781, 768, 750, 732, 714, 695, 693, 654, 624, 594, 555],
+	LocationY: [288, 286, 293, 310, 368, 425, 436, 468, 506, 497, 486, 489, 500, 506, 514, 531, 552, 523, 500, 484, 474],
+};
+
+//Recent crime records.
+
+var robberyLogbook_PositionX = [403, 402, 427, 646, 639, 830, 809, 844, 802, 683, 552, 629, 712, 783, 415, 561, 562, 751, 680, 626, 701, 838, 322, 468, 625];
+var robberyLogbook_PositionY = [401, 360, 403, 284, 264, 434, 443, 496, 350, 413, 464, 498, 562, 603, 225, 282, 392, 283, 359, 436, 455, 565, 508, 556, 737];
+
+
+function preload()
+{
+	countyMap = loadImage("map.png")
+}
+
+function setup()
+{
+	createCanvas(countyMap.width, countyMap.height);
+	noFill();
+	noStroke();
+	image(countyMap, 0,0);
+
+	//add your code below here
+
+    noStroke();
+    fill(199,21,133);
+   
+    for(var i =0; i< fryLog.LocationX[]; i++)
+        {
+            
+            
+            ellipse(fryLog.LocationX[i], fryLog.LocationY[i]);
+            
+        }
+     
+    noStroke();
+    fill(34,139,34);
+   
+    for(var i =0; i< robberyLogbook_PositionX.length; i++)
+        {
+            
+            
+            triangle(robberyLogbook_PositionX[i], robberyLogbook_PositionY[i]);
+            
+        }
+     
+    
+   
+
+}
+
+//We are not using the draw function this time
